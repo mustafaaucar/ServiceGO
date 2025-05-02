15da1d447d6ca52d1daefe87fa657d9b2b2e1f12
Mustafa UÇAR <67073181+mustafaaucar@users.noreply.github.com>
2025-05-02 17:43:10 +0300

    updated

--- ServiceGO/Controllers/RouteController.cs
+++ ServiceGO/Controllers/RouteController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ServiceGO.Controllers
 {
     public class RouteController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
+        private readonly IRoutesService _routeService;
+        private readonly IMapper _mapper;
+        public RouteController(IRoutesService routeService, IMapper mapper)
         {
-            return View();
+            _routeService = routeService;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var listPermission = User.Claims.FirstOrDefault(c => c.Type == "ListPermission");
+            if (listPermission != null && Convert.ToBoolean(listPermission.Value) == true)
+            {
+                var companyIdClaim = User.Claims.FirstOrDefault(c => c.Type == "CompanyID");
+                int companyID = int.Parse(companyIdClaim.Value);
+                var routes = await _routeService.GetCompanyRoutes(companyID);
+                return View(routes);
+            }
+            else
+            {
+                return Redirect("/Home/Login");
+            }
         }
     }
 }
